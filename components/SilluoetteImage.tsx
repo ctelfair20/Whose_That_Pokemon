3a31e92ecdaf4f6163b5da94314eecd4121ed4c1
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-15 14:14:16 -0600

    removes styles from silluoette comp

--- components/SilluoetteImage.tsx
+++ components/SilluoetteImage.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import styles from "../styles/WireFrame.module.css";
-
 
 const Silluoette = () => {
   return (
-    <Image className={styles.image} src="https://external-preview.redd.it/e5zoQw-hgw-LCjdhC_4G8IAcHxex5pzda_BD_FPTcBY.png?auto=webp&s=c0b96b5ec20010a15864b8a0c9b202c119e52fe8" alt="pokemon silluoette" width={100} height={100} />
+    <Image src="https://external-preview.redd.it/e5zoQw-hgw-LCjdhC_4G8IAcHxex5pzda_BD_FPTcBY.png?auto=webp&s=c0b96b5ec20010a15864b8a0c9b202c119e52fe8" alt="pokemon silluoette" width={100} height={100} />
   );
 }
 
