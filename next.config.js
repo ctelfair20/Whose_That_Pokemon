18db9f09ea89ad6836eaf2fd46cb00f9d004418e
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-15 14:42:46 -0600

    gives next.config the proper object for Image comp

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      // "https://external-preview.redd.it/e5zoQw-hgw-LCjdhC_4G8IAcHxex5pzda_BD_FPTcBY.png?auto=webp&s=c0b96b5ec20010a15864b8a0c9b202c119e52fe8"
+      {
+        protocol: "https",
+        hostname: "external-preview.redd.it",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
