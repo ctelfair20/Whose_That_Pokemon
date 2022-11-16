88f0e14d9d523e5cc4249c237e0fc5c4471cb470
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-16 13:32:49 -0600

    import axios

--- components/PokeImage.tsx
+++ components/PokeImage.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import styles from '../styles/PokeImage.module.css';
 
 const PokeImage = () => {
@@ -6,4 +7,19 @@ const PokeImage = () => {
   );
 }
 
-export default PokeImage;
+export async function getStaticProps() {
+  // Call an external API endpoint to get a pokemon.
+  // You can use any data fetching library
+  const res = await axios('https://pokeapi.co/api/v2/pokemon/ditto')
+  // const pokemon = await res.json();
+
+  // By returning { props: { pokemon } }, the Blog component
+  // will receive `pokemon` as a prop at build time
+  return {
+    props: {
+      pokemon,
+    },
+    //   }
+    // }
+
+    export default PokeImage;
