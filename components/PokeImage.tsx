787a6923fca3dc892e9ffdf25ab14cdf10e40b89
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-16 16:15:21 -0600

    used next Image to display image from props

--- components/PokeImage.tsx
+++ components/PokeImage.tsx
@@ -1,10 +1,18 @@
-import styles from '../styles/PokeImage.module.css';
+import Image from 'next/image'
 import { Props } from '../pages/index'
+import styles from '../styles/PokeImage.module.css'
 
 const PokeImage = ({ pokemon }: Props) => {
-
+  // console.log('poke', pokemon.sprites.other['official-artwork']);
   return (
-    <div className={styles.image}>image goes here</div>
+    <div className={styles.image}>
+      <Image
+        src={pokemon.sprites.other['official-artwork']['front_default']}
+        alt='hidden pokemon'
+        width={100}
+        height={100}
+      />
+    </div>
   );
 }
 
