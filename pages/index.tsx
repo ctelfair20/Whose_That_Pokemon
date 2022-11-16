cb4775fe4a489f3387a79e35d88e4abae55b0cb0
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-16 16:14:19 -0600

    fixed typo in sprites

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import BackgroundImage from '../components/BackgroundImage'
 
 export type Props = {
-  pokemon: Pokemon[]
+  pokemon: Pokemon
 }
 
 export type Pokemon = {
@@ -14,7 +14,7 @@ export type Pokemon = {
 
 export type Sprite = {
   other: {
-    'offical-artwork': {
+    'official-artwork': {
       front_default: string
     }
   }
@@ -36,7 +36,7 @@ export default function Home({ pokemon }: Props) {
 
 export const getStaticProps = async () => {
   // Call an external API endpoint to get a pokemon.
-  const res = await axios.get<Pokemon[]>('https://pokeapi.co/api/v2/pokemon/ditto');
+  const res = await axios.get<Pokemon>('https://pokeapi.co/api/v2/pokemon/ditto');
   const pokemon = res.data;
 
   // By returning { props: { pokemon } }, the PokemonImage component will receive `pokemon` as a prop at build time
