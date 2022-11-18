2a4dbcf82ec014838d595f2b6c56a03f20be5567
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-17 18:15:55 -0600

    iterates over and displays choices in choiceBox

--- components/ChoiceBox.tsx
+++ components/ChoiceBox.tsx
@@ -3,9 +3,13 @@ import Choice from './Choice';
 
 const ChoiceBox = ({ pokemonArray }: Props) => {
 
+  const allChoices = pokemonArray.map((pokemon) => {
+    return pokemon.name;
+  });
+
   return (
     <div>
-
+      {allChoices}
     </div>
   );
 }
