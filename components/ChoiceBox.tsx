44679e883a0dabe9ba461ec9c35140681539fdab
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-17 18:12:21 -0600

    creates choiceBox

---
+++ components/ChoiceBox.tsx
@@ -0,0 +1,13 @@
+import { Props } from '../pages/index'
+import Choice from './Choice';
+
+const ChoiceBox = ({ pokemonArray }: Props) => {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default ChoiceBox;
