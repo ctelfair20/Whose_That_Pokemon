dbad44063b8a5c3dbae687800469d98c2aad27f1
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-21 16:05:43 -0600

    capitalizes the first letter of pokemon name

--- components/choice.tsx
+++ components/choice.tsx
@@ -6,22 +6,22 @@ type Props = {
   pokemon: Pokemon
 }
 
-const handleClick = () => {
-  // when clicked, toggle border color
-  // check if selected pokemon is correct
-  // render a green border
-  // else
-  // render a red border
-}
-
 const Choice = ({ pokemon }: Props) => {
-
+  const CapsName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
   const [isCorrect, setIsCorrect] = useState(false);
+
+  const handleClick = () => {
+    // when clicked, toggle border color
+    // check if selected pokemon is correct
+    // render a green border
+    // else
+    // render a red border
+  }
 
   return (
     isCorrect ?
       <div className={styles.correct}> {pokemon.name} </div> :
-      <div className={styles.choice}> {pokemon.name} </div>
+      <div className={styles.choice}> {CapsName} </div>
   );
 }
 
