4bc837f0d7f024c206499bb75ce0a4f3f2794eb5
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-21 15:26:05 -0600

    adds blue border to choices when hovered

--- components/choice.tsx
+++ components/choice.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react'
 import { Pokemon } from '../pages/index'
 import styles from '../styles/Choice.module.css'
 
 type Props = {
   pokemon: Pokemon
 }
+
+const handleClick = () => {
+  // when clicked, toggle border color
+  // check if selected pokemon is correct
+  // render a green border
+  // else
+  // render a red border
+}
+
 const Choice = ({ pokemon }: Props) => {
+
+  const [isCorrect, setIsCorrect] = useState(false);
+
   return (
-    <div className={styles.choice}>
-      {pokemon.name}
-    </div>
+    isCorrect ?
+      <div className={styles.correct}> {pokemon.name} </div> :
+      <div className={styles.choice}> {pokemon.name} </div>
   );
 }
 
