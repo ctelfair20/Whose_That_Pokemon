978909c5fefe93aa95cb03af9e7d53ffcdbfd14e
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-22 11:14:13 -0600

    Displays difference styled choice based on correctness

--- components/choice.tsx
+++ components/choice.tsx
@@ -9,23 +9,37 @@ type Props = {
 
 const Choice = ({ pokemon, pokemonArray }: Props) => {
   const CapsName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-  const [isCorrect, setIsCorrect] = useState(false);
+  const [isCorrect, setIsCorrect] = useState('not selected');
 
   const handleClick = () => {
     // when clicked, toggle border color
     // check if selected pokemon is correct
-    // if selected pokemon is equal to the first pokemon in
-    // render a green border
-    // change isCorrect to true
-    // else
-    // render a red border
-    // change isCorrect to false
+    // if selected pokemon is equal to the first pokemon in pokemonArray
+    if (pokemon.name === pokemonArray[0].name) {
+      // render a green border
+      // change isCorrect to true
+      setIsCorrect('correct');
+      // else
+    } else {
+      // render a red border
+      // change isCorrect to false
+      setIsCorrect('wrong');
+    }
+  }
+
+  const choiceStyle = () => {
+    if (isCorrect === 'not selected') {
+      return <div className={styles.choice} onClick={handleClick}> {CapsName} </div>
+    } else if (isCorrect === 'correct') {
+      return <div className={styles.correct} onClick={handleClick}> {CapsName} </div>
+    } else {
+      return <div className={styles.wrong} onClick={handleClick}> {CapsName} </div>
+    }
   }
 
   return (
-    isCorrect ?
-      <div className={styles.correct}> {pokemon.name} </div> :
-      <div className={styles.choice}> {CapsName} </div>
+    // why is this working without curly brackets??
+    choiceStyle()
   );
 }
 
