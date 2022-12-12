36df89d6219b4247ef86b133af0316a5021e776f
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-12-12 11:03:00 -0600

    changes choice font size

--- components/ChoiceBox.tsx
+++ components/ChoiceBox.tsx
@@ -1,11 +1,11 @@
 import { Props } from '../pages/index'
 import stlyes from '../styles/Choice.module.css'
-import Choice from './Choice';
-
+import Choice from './Choice'
 
 const ChoiceBox = ({ pokemonArray }: Props) => {
 
   const allChoices = (start: number, end = 4) => {
+    // need to randomize choices so that the answer isn't in the same spot every time.
     return pokemonArray.map((pokemon, i) => {
       if (i >= start && i < end) {
         return <Choice key={pokemon.id} pokemonArray={pokemonArray} pokemon={pokemon} />
