4b0140fd34e442276fb1620cf03e77d5abe3d8cf
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-12-15 19:49:50 -0600

    test2

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Background.module.css';
 const BackgroundImage = ({ pokemonArray }: Props) => {
   // console.log('pokeB', pokemonArray);
   return (
-    // to decrease pixelation in remote images, increase the height and width propertie
+    // to decrease pixelation in remote images,  increase the height and width propertie
     <>
       <Image className={styles.background} src="https://i0.wp.com/www.alphr.com/wp-content/uploads/2016/07/whos_that_pokemon.png?fit=1920%2C1080&ssl=1" alt="pokemon silluoette" width={800} height={800} priority />
       <PokeImage pokemonArray={pokemonArray} />
