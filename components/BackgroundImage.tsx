12440e40111911c7762a34d16ea5c28417de1501
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-01-11 20:23:38 -0600

    passes shouldBeVisible and setShouldBeVisible down to childcomponents

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import PokeImage from "./PokeImage";
 import { Pokemon } from "../pages/index";
@@ -18,14 +18,10 @@ const BackgroundImage = ({ pokemonArray, setPokemonArr }: BackgroundProps) => {
     // to decrease pixelation in remote images, increase the height and width properties
     <>
       <Image className={styles.background} src="https://i0.wp.com/www.alphr.com/wp-content/uploads/2016/07/whos_that_pokemon.png?fit=1920%2C1080&ssl=1" alt="pokemon silluoette" width={800} height={800} priority />
-      <PokeImage pokemonArray={pokemonArray} />
-      <ChoiceBox pokemonArray={pokemonArray} setPokemonArr={setPokemonArr} />
+      <PokeImage pokemonArray={pokemonArray} shouldBeVisible={shouldBeVisible} />
+      <ChoiceBox pokemonArray={pokemonArray} setPokemonArr={setPokemonArr} setShouldBeVisible={setShouldBeVisible} />
     </>
   );
 }
 
 export default BackgroundImage;
-
-function useState(arg0: boolean): [any, any] {
-  throw new Error("Function not implemented.");
-}
