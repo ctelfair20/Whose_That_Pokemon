3318cea5cc40df61d2861d873d688fa239fe74b9
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-01-11 20:28:47 -0600

    sets shouldBeVisible to true when the right choice is picked or when three wrong choices are picked

--- components/PokeImage.tsx
+++ components/PokeImage.tsx
@@ -1,43 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
-import { Props } from '../pages/index'
+import { Pokemon } from '../pages'
 import styles from '../styles/PokeImage.module.css'
 
-const PokeImage = ({ pokemonArray }: Props) => {
-
-  // give image a state === shouldBeVisible
-  // if that state is false
-  if (!shouldBeVisible) {
-    // show black version
-    return (
-      <div className={styles.silhouette}>
-        <Image
-          src={pokemonArray[0].sprites.other['official-artwork']['front_default']}
-          alt='hidden pokemon'
-          width={400}
-          height={400}
-        />
-      </div>
-    )
-    // if that state is true
-  } else {
-    // show normal version
-    return (
-      <div className={styles.original}>
-        <Image
-          src={pokemonArray[0].sprites.other['official-artwork']['front_default']}
-          alt='hidden pokemon'
-          width={400}
-          height={400}
-        />
-      </div>
-    )
-  }
+interface Props {
+  pokemonArray: Pokemon[]
+  shouldBeVisible: boolean
 }
 
-return (
-  showOrNoShow()
-);
+const PokeImage = ({ pokemonArray, shouldBeVisible }: Props) => {
+
+  const showOrNoShow = () => {
+    // give image a state === shouldBeVisible
+    // if that state is false
+    if (!shouldBeVisible) {
+      // show black version
+      return (
+        <div className={styles.silhouette}>
+          <Image
+            src={pokemonArray[0].sprites.other['official-artwork']['front_default']}
+            alt='hidden pokemon'
+            width={400}
+            height={400}
+          />
+        </div>
+      )
+      // if that state is true
+    } else {
+      // show normal version
+      return (
+        <div className={styles.original}>
+          <Image
+            src={pokemonArray[0].sprites.other['official-artwork']['front_default']}
+            alt='hidden pokemon'
+            width={400}
+            height={400}
+          />
+        </div>
+      )
+    }
+  }
+
+  return (
+    showOrNoShow()
+  );
 }
 
 export default PokeImage;
