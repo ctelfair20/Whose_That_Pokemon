6652614fad46ab510a800e214137319fefce1f06
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-01-11 19:52:47 -0600

    makes pokemon image render based on shouldBeVisible

--- components/PokeImage.tsx
+++ components/PokeImage.tsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { Props } from '../pages/index'
 import styles from '../styles/PokeImage.module.css'
 
 const PokeImage = ({ pokemonArray }: Props) => {
-  return (
-    <div className={styles.image}>
-      <Image
-        src={pokemonArray[0].sprites.other['official-artwork']['front_default']}
-        alt='hidden pokemon'
-        width={400}
-        height={400}
-      />
-    </div>
-  );
+
+  // give image a state === shouldBeVisible
+  // if that state is false
+  if (!shouldBeVisible) {
+    // show black version
+    return (
+      <div className={styles.silhouette}>
+        <Image
+          src={pokemonArray[0].sprites.other['official-artwork']['front_default']}
+          alt='hidden pokemon'
+          width={400}
+          height={400}
+        />
+      </div>
+    )
+    // if that state is true
+  } else {
+    // show normal version
+    return (
+      <div className={styles.original}>
+        <Image
+          src={pokemonArray[0].sprites.other['official-artwork']['front_default']}
+          alt='hidden pokemon'
+          width={400}
+          height={400}
+        />
+      </div>
+    )
+  }
+}
+
+return (
+  showOrNoShow()
+);
 }
 
 export default PokeImage;
