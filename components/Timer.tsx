0a76ab6e1ba2190b962ce37c83c171199d6555c7
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-01-14 13:00:13 -0600

    created a timer until the next game should start

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Timer.module.css';
+import { fetchFourPokemon } from '../helperFunctions';
 
 interface Props {
   gameOver: boolean
+  // setPokemonArray: () => void
 }
 
 const Timer = ({ gameOver }: Props) => {
+
+  const [countDown, setCountDown] = useState(3);
+
+  const timeUntilNextGame = () => {
+    if (gameOver && countDown > 0) {
+      setTimeout(() => {
+        // console.log(countDown);
+        setCountDown(countDown - 1);
+      }, 1000);
+    }
+    // const newPokemonArray = fetchFourPokemon(setPokemonArray);
+    return countDown;
+  }
+
   return (
     <>
       {
         gameOver &&
         <div className={styles['timer-box']}>
           <div className={styles['timer-text']}>
-            You Won! Next Pokemon in...
+            {`You Won! Next Pokemon in...${timeUntilNextGame()}`}
           </div>
         </div>
       }
