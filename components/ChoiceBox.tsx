ff4de9fe75014fbc468157b0b5d5d15cda09ae28
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-01-13 23:07:29 -0600

    moves logic heavy functions to helperFunctions file

--- components/ChoiceBox.tsx
+++ components/ChoiceBox.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import Choice from './Choice'
 import { Pokemon } from '../pages/index'
+import { randomizeChoices } from '../helperFunctions'
 import stlyes from '../styles/Choice.module.css'
-
-interface SlotsI {
-  [slot: number]: null | Pokemon
-}
 
 interface Props {
   pokemonArray: Pokemon[]
@@ -43,38 +40,9 @@ const ChoiceBox = ({ pokemonArray, setShouldBeVisible, gameOver, setGameOver }: 
   }, [status])
 
   useEffect(() => {
-    const mixed = randomizeChoices();
+    const mixed = randomizeChoices(pokemonArray);
     setMixedArray(mixed);
   }, [])
-
-  const randomizeChoices = () => {
-    // takes in an array of pokemon - [{A}, {B}, {C}, {D}]
-    // creates an array that holds the possible indices - [0,1,2,3]
-    const indicesArray: number[] = [0, 1, 2, 3];
-    // create an empty obj - {0: null,...}
-    const slots: SlotsI = { 0: null, 1: null, 2: null, 3: null };
-    // create empty array to repopulate with chioces in diff order
-    let mixed: Pokemon[] = [];
-    // while index array still has elements inside
-    while (indicesArray.length) {
-
-      // get a number between 0 and 3 -  random index
-      const random: number = Math.floor(Math.random() * 4);
-      // check if obj at random index is null
-      if (slots[random] === null) {
-        // pop off the last number
-        const popped: number | undefined = indicesArray.pop();
-        // use that popped number to access a pokemon and fill the slot
-        if (typeof popped === 'number') {
-          slots[random] = pokemonArray[popped];
-          // add pokemon to the empty array at the random index
-          mixed[random] = pokemonArray[popped];
-        }
-      }
-    }
-    // return the empty array of mixed pokemon - [{B}, {D}, {A}, {C}]
-    return mixed;
-  }
 
   const allChoices = (start: number, mixedArray: Pokemon[], end = 4) => {
     // need to randomize choices so that the answer isn't in the same spot every time.
--- helperFunctions.tsx
+++ helperFunctions.tsx
@@ -1,6 +1,39 @@
 import axios from "axios";
 import { Pokemon } from './pages/index';
 
+interface SlotsI {
+  [slot: number]: null | Pokemon
+}
+
+export const randomizeChoices = (pokemonArray: Pokemon[]) => {
+  // takes in an array of pokemon - [{A}, {B}, {C}, {D}]
+  // creates an array that holds the possible indices - [0,1,2,3]
+  const indicesArray: number[] = [0, 1, 2, 3];
+  // create an empty obj - {0: null,...}
+  const slots: SlotsI = { 0: null, 1: null, 2: null, 3: null };
+  // create empty array to repopulate with chioces in diff order
+  let mixed: Pokemon[] = [];
+  // while index array still has elements inside
+  while (indicesArray.length) {
+
+    // get a number between 0 and 3 -  random index
+    const random: number = Math.floor(Math.random() * 4);
+    // check if obj at random index is null
+    if (slots[random] === null) {
+      // pop off the last number
+      const popped: number | undefined = indicesArray.pop();
+      // use that popped number to access a pokemon and fill the slot
+      if (typeof popped === 'number') {
+        slots[random] = pokemonArray[popped];
+        // add pokemon to the empty array at the random index
+        mixed[random] = pokemonArray[popped];
+      }
+    }
+  }
+  // return the empty array of mixed pokemon - [{B}, {D}, {A}, {C}]
+  return mixed;
+}
+
 /**
  * randomNumber generates a number based on the generation of pokemon
  * returns a number within that generation's range
